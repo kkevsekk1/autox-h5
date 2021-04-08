63423c95cb10e496411db6fba39558418214a27e
bird-xzc <987500737@qq.com>
2021-04-08 14:30:53 +0800

    增加mine页面

---
+++ pages/mine/mine.vue
@@ -0,0 +1,24 @@
+<template>
+	<view class="login-box">
+		<navigator url="/pages/login/login">
+			<button  class= "button" type="default">退出登录</button>
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+	.login-box {
+		padding: 40rpx;
+		font-size: 28rpx;
+	}
+	.login-box .button {
+		background-color: #007AFF;
+		color: white;
+	}
+</style>
