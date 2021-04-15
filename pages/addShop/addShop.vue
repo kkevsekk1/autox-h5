7d034ae6934a7e95697d5a6ffa60a6719283be97
toinghub <228530402@qq.com>
2021-04-15 15:40:49 +0800

    样式更改

--- pages/addShop/addShop.vue
+++ pages/addShop/addShop.vue
@@ -1,224 +1,244 @@
 <template>
-	<view class="addShop_box">
-		<button class="addBtn" @click="addShop" type="default">添加商铺</button>
-		<view v-for="item in shopList" :key="item.id">
-			<view class="shop-list">
-				<view class="content">
-					<view>店铺名称：{{ item.name }}</view>
-					<view>店铺电话：{{ item.account }}</view>
-				</view>
-			</view>
-		</view>
+  <view class="addShop_box">
+    <button class="addBtn"
+            @click="addShop"
+            type="default">添加商铺</button>
+    <view v-for="item in shopList"
+          :key="item.id">
+      <view class="shop-list">
+        <view class="content">
+          <view>店铺名称：{{ item.name }}</view>
+          <view>店铺电话：{{ item.account }}</view>
+        </view>
+      </view>
+    </view>
 
-		<uni-popup ref="popup" type="center">
-			<uni-forms :value="user" ref="form" :rules="rules" class="addshop">
-				<uni-forms-item label="商铺名" name="name" required>
-					<uni-easyinput type="text" v-model="user.name" placeholder="请输入商铺名" />
-				</uni-forms-item>
-				<uni-forms-item label="密码" name="password" required>
-					<uni-easyinput type="password" v-model="user.password" placeholder="请输入密码" />
-				</uni-forms-item>
-				<uni-forms-item label="电话号码" name="account" required>
-					<uni-easyinput type="text" v-model="user.account" placeholder="请输入电话号码" />
-				</uni-forms-item>
-				<button @click="submitForm" type="primary">提交</button>
-			</uni-forms>
-		</uni-popup>
-	</view>
+    <uni-popup ref="popup"
+               type="center">
+      <uni-forms :value="user"
+                 ref="form"
+                 :rules="rules"
+                 class="addshop">
+        <uni-forms-item label="商铺名"
+                        name="name"
+                        required>
+          <uni-easyinput type="text"
+                         v-model="user.name"
+                         placeholder="请输入商铺名" />
+        </uni-forms-item>
+        <uni-forms-item label="密码"
+                        name="password"
+                        required>
+          <uni-easyinput type="password"
+                         v-model="user.password"
+                         placeholder="请输入密码" />
+        </uni-forms-item>
+        <uni-forms-item label="电话号码"
+                        name="account"
+                        required>
+          <uni-easyinput type="text"
+                         v-model="user.account"
+                         placeholder="请输入电话号码" />
+        </uni-forms-item>
+        <button @click="submitForm"
+                type="primary">提交</button>
+      </uni-forms>
+    </uni-popup>
+  </view>
 </template>
 <script>
-	import {
-		request
-	} from '../../server/request.js'
-	export default {
-		data() {
-			return {
-				shopList: [],
-				pages: {
-					index: 1,
-					size: 10,
-					count: 0,
-				},
-				user: {
-					account: '',
-					password: '',
-					name: ''
-				},
-				rules: {
-					account: {
-						rules: [{
-								required: true,
-								errorMessage: '请输入正确的电话号码'
-							},
-							{
-								length: 11,
-							},
-						],
-					},
-					password: {
-						rules: [{
-								required: true,
-								errorMessage: '请输入密码'
-							},
-							{
-								minLength: 6,
-								errorMessage: '密码不得少于6位数',
-							},
-						],
-					},
-					name: {
-						rules: [{
-							required: true,
-							errorMessage: '请输入正确的店铺名'
-						}],
-					},
-				},
-			}
-		},
-		created() {
-			this.initialData()
-		},
-		onReachBottom() {
-			// 当前页大于等于总页数
-			if (this.pages.index >= this.pages.count) {
-				uni.showToast({
-					title: '到底啦',
-					icon: 'none',
-				})
-			} else {
-				this.pages.index++
-				this.getShopList()
-			}
-			// 进入下一页
-		},
-		onPullDownRefresh() {
-			this.initialData()
-			setTimeout(() => {
-				uni.stopPullDownRefresh()
-			}, 1000)
-		},
-		methods: {
-			initialData() {
-				this.getShopList()
-			},
-			getShopList() {
-				request({
-					url: '/auth/getShopPage',
-					method: 'post',
-					data: {
-						index: this.pages.index.toString(),
-						size: '10',
-						search: '',
-						orderby: 'id desc',
-					},
-				}).then((loadresult) => {
-					console.log(loadresult.data)
-					uni.hideLoading()
-					const {
-						message,
-						code,
-						data
-					} = loadresult.data
-					if (code === 200) {
-						this.pages = {
-							count: data.pages,
-							index: data.index,
-							size: data.size,
-						}
-						console.log(this.pages)
-						loadresult.data.data.list.forEach((item) => {
-							this.shopList.push(item)
-						})
-					}
-				})
-			},
-			addShop() {
-				this.$refs.popup.open()
-			},
-			submitForm(form) {
-				this.$refs.form.submit().then((res) => {
-					console.log(res)
-					request({
-						url: '/auth/addShop',
-						method: 'post',
-						data: res,
-					}).then((loadresult) => {
-						let {
-							code,
-							message,
-							data
-						} = loadresult.data
-						console.log(loadresult)
-					})
-				})
-				this.$router.go(0)
-			},
-		},
-	}
+import {
+  request
+} from '../../server/request.js'
+export default {
+  data () {
+    return {
+      shopList: [],
+      pages: {
+        index: 1,
+        size: 10,
+        count: 0,
+      },
+      user: {
+        account: '',
+        password: '',
+        name: ''
+      },
+      rules: {
+        account: {
+          rules: [{
+            required: true,
+            errorMessage: '请输入正确的电话号码'
+          },
+          {
+            length: 11,
+          },
+          ],
+        },
+        password: {
+          rules: [{
+            required: true,
+            errorMessage: '请输入密码'
+          },
+          {
+            minLength: 6,
+            errorMessage: '密码不得少于6位数',
+          },
+          ],
+        },
+        name: {
+          rules: [{
+            required: true,
+            errorMessage: '请输入正确的店铺名'
+          }],
+        },
+      },
+    }
+  },
+  created () {
+    this.initialData()
+  },
+  onReachBottom () {
+    // 当前页大于等于总页数
+    if (this.pages.index >= this.pages.count) {
+      uni.showToast({
+        title: '到底啦',
+        icon: 'none',
+      })
+    } else {
+      this.pages.index++
+      this.getShopList()
+    }
+    // 进入下一页
+  },
+  onPullDownRefresh () {
+    this.initialData()
+    setTimeout(() => {
+      uni.stopPullDownRefresh()
+    }, 1000)
+  },
+  methods: {
+    initialData () {
+      this.getShopList()
+    },
+    getShopList () {
+      request({
+        url: '/auth/getShopPage',
+        method: 'post',
+        data: {
+          index: this.pages.index.toString(),
+          size: '10',
+          search: '',
+          orderby: 'id desc',
+        },
+      }).then((loadresult) => {
+        console.log(loadresult.data)
+        uni.hideLoading()
+        const {
+          message,
+          code,
+          data
+        } = loadresult.data
+        if (code === 200) {
+          this.pages = {
+            count: data.pages,
+            index: data.index,
+            size: data.size,
+          }
+          console.log(this.pages)
+          loadresult.data.data.list.forEach((item) => {
+            this.shopList.push(item)
+          })
+        }
+      })
+    },
+    addShop () {
+      this.$refs.popup.open()
+    },
+    submitForm (form) {
+      this.$refs.form.submit().then((res) => {
+        console.log(res)
+        request({
+          url: '/auth/addShop',
+          method: 'post',
+          data: res,
+        }).then((loadresult) => {
+          let {
+            code,
+            message,
+            data
+          } = loadresult.data
+          console.log(loadresult)
+        })
+      })
+      this.$router.go(0)
+    },
+  },
+}
 </script>
 <style scoped>
-	page {
-		background-color: #f8f8f8;
-	}
+page {
+  background-color: #f8f8f8;
+}
 
-	.addShop_box {
-		position: relative;
-		padding: 20rpx;
-		font-size: 30rpx;
-		background-color: #f5f5f5;
-	}
+.addShop_box {
+  position: relative;
+  padding: 20rpx;
+  font-size: 30rpx;
+  background-color: #f5f5f5;
+}
 
-	.addShop_box .addBtn {
-		background-color: #007aff;
-		color: white;
-		font-size: 32rpx;
-		height: 100rpx;
-		line-height: 100rpx;
-	}
+.addShop_box .addBtn {
+  background-color: #007aff;
+  color: white;
+  font-size: 32rpx;
+  height: 100rpx;
+  line-height: 100rpx;
+}
 
-	.addShop_box .addshop {
-		box-sizing: border-box;
-		width: 650rpx;
-		height: 500rpx;
-		background-color: white;
-		padding: 40rpx 30rpx;
-		border: 1rpx #999999 solid;
-		border-radius: 30rpx;
-	}
+.addShop_box .addshop {
+  box-sizing: border-box;
+  width: 650rpx;
+  height: 500rpx;
+  background-color: white;
+  padding: 40rpx 30rpx;
+  border: 1rpx #999999 solid;
+  border-radius: 30rpx;
+}
 
-	::v-deep .uni-forms-item__inner {
-		padding-bottom: 36rpx;
-	}
+::v-deep .uni-forms-item__inner {
+  padding-bottom: 36rpx;
+}
 
-	::v-deep .uni-forms-item__label {
-		width: 150rpx !important;
-	}
+::v-deep .uni-forms-item__label {
+  width: 150rpx !important;
+}
 
-	::v-deep .uni-forms-item__label .label-text {
-		font-size: 28rpx;
-		color: rgb(22, 22, 22);
-		width: 120rpx;
-		text-align-last: justify;
-	}
+::v-deep .uni-forms-item__label .label-text {
+  font-size: 28rpx;
+  color: rgb(22, 22, 22);
+  width: 120rpx;
+  text-align-last: justify;
+}
 
-	::v-deep .uni-input-wrapper .uni-input-placeholder {
-		color: #a7a7a7;
-		font-size: 24rpx;
-	}
+::v-deep .uni-input-wrapper .uni-input-placeholder {
+  color: #a7a7a7;
+  font-size: 24rpx;
+}
 
-	.shop-list {
-		height: 148rpx;
-		margin: 24rpx 0;
-		border-radius: 10rpx;
-		background-color: #fff;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-	}
+.shop-list {
+  height: 148rpx;
+  margin: 24rpx 0;
+  border-radius: 10rpx;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
 
-	.content view {
-		height: 58rpx;
-		line-height: 58rpx;
-		padding-left: 22rpx;
-	}
+.content view {
+  height: 58rpx;
+  line-height: 58rpx;
+  padding-left: 22rpx;
+}
 </style>
--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,221 +1,223 @@
 <template>
-	<view class="login-box">
-		<view>
-			<header>
-				<view class="bead">
-					<text class="head-portrait">
-						<img :src="dataMine.head" alt="" />
-					</text>
-					<text> ID: {{ dataMine.phone }}</text>
-				</view>
-				<view class="nav">
-					<text>
-						<p>{{ dataMine.balance }}</p>
-						金额
-					</text>
-					<text>
-						<p>{{ dataMine.performance }}</p>
-						总提成
-					</text>
-					<text>
-						<p>{{ dataMine.pfBalance }}</p>
-						提成金额
-					</text>
-				</view>
-			</header>
-			<article>
-				<view>
-					会员号<text>{{ dataMine.member }}</text>
-				</view>
-				<view>
-					code<text>{{ dataMine.code }}</text>
-				</view>
-				<view>
-					到期时间<text>{{ dataMine.time }}</text>
-				</view>
-				<navigator url="/pages/login/login">
-					<button class="button" type="default">退出登录</button>
-				</navigator>
-			</article>
-		</view>
-	</view>
+  <view class="login-box">
+    <view>
+      <header>
+        <view class="bead">
+          <text class="head-portrait">
+            <img :src="dataMine.head"
+                 alt="" />
+          </text>
+          <text> ID: {{ dataMine.phone }}</text>
+        </view>
+        <view class="nav">
+          <text>
+            <p>{{ dataMine.balance }}</p>
+            金额
+          </text>
+          <text>
+            <p>{{ dataMine.performance }}</p>
+            总提成
+          </text>
+          <text>
+            <p>{{ dataMine.pfBalance }}</p>
+            提成金额
+          </text>
+        </view>
+      </header>
+      <article>
+        <view>
+          会员号<text>{{ dataMine.member }}</text>
+        </view>
+        <view>
+          code<text>{{ dataMine.code }}</text>
+        </view>
+        <view>
+          到期时间<text>{{ dataMine.time }}</text>
+        </view>
+        <navigator url="/pages/login/login">
+          <button class="button"
+                  type="default">退出登录</button>
+        </navigator>
+      </article>
+    </view>
+  </view>
 </template>
 
 <script>
-	import {
-		request
-	} from '../../server/request.js'
-	export default {
-		created() {
-			this.getMine()
-		},
-		data() {
-			return {
-				dataMine: {
-					head: '../../static/uni.png',
-					phone: '112233',
-					balance: '1.22',
-					performance: '333',
-					pfBalance: '3334',
-					member: '24',
-					code: '25',
-					time: '2020-01-01',
-					surplus: '88888',
-				},
-			}
-		},
-		methods: {
-			onPullDownRefresh() {
-				this.initialData()
-				setTimeout(() => {
-					uni.stopPullDownRefresh()
-				}, 1000)
-			},
-			initialData() {
-				this.getMine()
-			},
-			getMine() {
-				request({
-					url: '/auth/info',
-					method: 'get',
-					data: '',
-				}).then((loadresult) => {
-					let {
-						message,
-						code,
-						data
-					} = loadresult.data
-					console.log(loadresult.data)
-					if (code === 200) {
-						let dataMines = this.dataMine
-						dataMines.phone = data.account
-						dataMines.balance = data.balance
-						dataMines.performance = data.commissionBalance
-						dataMines.pfBalance = data.commissionBalance + data.balance
-						dataMines.member = data.id
-						dataMines.code = data.code
-						dataMines.time = data.vipExpirationTime.substring(0, 10)
-						dataMines.surplus = data.scanningTimes
-					}
-				})
-			},
-		},
-	}
+import {
+  request
+} from '../../server/request.js'
+export default {
+  created () {
+    this.getMine()
+  },
+  data () {
+    return {
+      dataMine: {
+        head: '../../static/uni.png',
+        phone: '112233',
+        balance: '1.22',
+        performance: '333',
+        pfBalance: '3334',
+        member: '24',
+        code: '25',
+        time: '2020-01-01',
+        surplus: '88888',
+      },
+    }
+  },
+  methods: {
+    onPullDownRefresh () {
+      this.initialData()
+      setTimeout(() => {
+        uni.stopPullDownRefresh()
+      }, 1000)
+    },
+    initialData () {
+      this.getMine()
+    },
+    getMine () {
+      request({
+        url: '/auth/info',
+        method: 'get',
+        data: '',
+      }).then((loadresult) => {
+        let {
+          message,
+          code,
+          data
+        } = loadresult.data
+        console.log(loadresult.data)
+        if (code === 200) {
+          let dataMines = this.dataMine
+          dataMines.phone = data.account
+          dataMines.balance = data.balance
+          dataMines.performance = data.commissionBalance
+          dataMines.pfBalance = data.commissionBalance + data.balance
+          dataMines.member = data.id
+          dataMines.code = data.code
+          dataMines.time = data.vipExpirationTime.substring(0, 10)
+          dataMines.surplus = data.scanningTimes
+        }
+      })
+    },
+  },
+}
 </script>
 
 <style scoped>
-	.login-box {
-		background-color: #f5f5f5;
-		padding: 0 !important;
-	}
+.login-box {
+  background-color: #f5f5f5;
+  padding: 0 !important;
+}
 
-	header {
-		height: 430rpx;
-		background-color: #007aff;
-		color: #fff;
-		padding: 20rpx;
-	}
+header {
+  height: 430rpx;
+  background-color: #007aff;
+  color: #fff;
+  padding: 20rpx;
+}
 
-	.bead {
-		margin: 20rpx 0;
-	}
+.bead {
+  margin: 20rpx 0;
+}
 
-	.head-portrait {
-		margin: 0 auto;
-		width: 147rpx;
-		height: 147rpx;
-	}
+.head-portrait {
+  margin: 0 auto;
+  width: 147rpx;
+  height: 147rpx;
+}
 
-	.head-portrait img {
-		width: 100%;
-	}
+.head-portrait img {
+  width: 100%;
+}
 
-	header::after {
-		content: '';
-		clear: both;
-		display: block;
-	}
+header::after {
+  content: "";
+  clear: both;
+  display: block;
+}
 
-	header text {
-		display: block;
-		margin-top: 26rpx;
-		text-align: center;
-	}
+header text {
+  display: block;
+  margin-top: 26rpx;
+  text-align: center;
+}
 
-	.nav {
-		display: flex;
-		height: 160rpx;
-		margin: 56rpx 0 10rpx 0;
-	}
+.nav {
+  display: flex;
+  height: 160rpx;
+  margin: 56rpx 0 10rpx 0;
+}
 
-	.nav text {
-		flex: 1;
-		width: 33%;
-		font-size: 26rpx;
-		box-sizing: border-box;
-		font-weight: 200;
-	}
+.nav text {
+  flex: 1;
+  width: 33%;
+  font-size: 26rpx;
+  box-sizing: border-box;
+  font-weight: 200;
+}
 
-	.nav text p {
-		height: 50rpx;
-		margin-bottom: 10rpx;
-		font-size: 36rpx;
-		font-weight: 900;
-	}
+.nav text p {
+  height: 50rpx;
+  margin-bottom: 10rpx;
+  font-size: 36rpx;
+  font-weight: 900;
+}
 
-	.nav text:nth-child(2) {
-		position: relative;
-	}
+.nav text:nth-child(2) {
+  position: relative;
+}
 
-	.nav text:nth-child(2)::before,
-	.nav text:nth-child(2)::after {
-		content: '';
-		width: 1px;
-		height: 40rpx;
-		position: absolute;
-		top: 32rpx;
-		background-color: #6ab1ff;
-	}
+.nav text:nth-child(2)::before,
+.nav text:nth-child(2)::after {
+  content: "";
+  width: 1px;
+  height: 40rpx;
+  position: absolute;
+  top: 32rpx;
+  background-color: #6ab1ff;
+}
 
-	.nav text:nth-child(2)::before {
-		left: 0;
-	}
+.nav text:nth-child(2)::before {
+  left: 0;
+}
 
-	.nav text:nth-child(2)::after {
-		right: 0;
-	}
+.nav text:nth-child(2)::after {
+  right: 0;
+}
 
-	article {
-		margin: 22rpx;
-	}
+article {
+  margin: 22rpx;
+}
 
-	article view {
-		height: 100rpx;
-		line-height: 100rpx;
-		margin: 10rpx 0;
-		padding: 0 30rpx;
-		background-color: #fff;
-		font-size: 30rpx;
-		color: #333333;
-	}
+article view {
+  height: 100rpx;
+  line-height: 100rpx;
+  margin: 10rpx 0;
+  padding: 0 30rpx;
+  background-color: #fff;
+  font-size: 30rpx;
+  color: #333333;
+}
 
-	article text {
-		float: right;
-		color: #bbb;
-	}
+article text {
+  float: right;
+  color: #bbb;
+}
 
-	.login-box {
-		padding: 40rpx;
-		font-size: 28rpx;
-		padding-top: 5rpx;
-	}
+.login-box {
+  padding: 40rpx;
+  font-size: 28rpx;
+  padding-top: 5rpx;
+}
 
-	.login-box .button {
-		background-color: #007aff;
-		color: white;
-		margin-top: 44rpx;
-		font-size: 32rpx;
-		height: 100rpx;
-		line-height: 100rpx;
-	}
+.login-box .button {
+  background-color: #007aff;
+  color: white;
+  margin-top: 44rpx;
+  font-size: 32rpx;
+  height: 100rpx;
+  line-height: 100rpx;
+}
 </style>
