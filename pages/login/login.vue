658587a31d7e92af130d2f5848c5f02370d9c653
toinghub <228530402@qq.com>
2021-06-02 16:40:29 +0800

    登陆界面

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,26 +1,54 @@
 <template>
   <view class="login-box">
-    <uni-forms :value="user"
-               ref="form"
-               :rules="rules">
-      <h2 class="login-header">账号登陆</h2>
-      <uni-forms-item label="用户名"
-                      name="phone"
-                      required>
-        <uni-easyinput type="text"
-                       v-model="user.phone"
-                       placeholder="用户名" />
-      </uni-forms-item>
-      <uni-forms-item label="密码"
-                      name="verifyCode"
-                      required>
-        <uni-easyinput type="password"
-                       v-model="user.verifyCode"
-                       placeholder="密码" />
-      </uni-forms-item>
-      <button @click="submitForm"
-              type="primary">登陆</button>
-    </uni-forms>
+    <view class="login-logo">
+      <img src="../../static/logo.png"
+           alt="">
+    </view>
+    <view class="input-list">
+      <view :class="error['phone']?'error-phone':''">
+        <input placeholder-style="color:#b1acac"
+               type="text"
+               class="input-class"
+               v-model="user.phone"
+               placeholder="请输入手机号"
+               :class="error['phone']?'border-red':''" />
+      </view>
+      <uni-row>
+        <uni-col :span="16">
+          <view :class="error['verifyCode']?'error-verifyCode':''">
+            <input placeholder-style="color:#b1acac"
+                   type="text"
+                   class="input-class"
+                   v-model="user.verifyCode"
+                   placeholder="请输入密码/验证码"
+                   :class="error['verifyCode']?'border-red':''" />
+          </view>
+        </uni-col>
+        <uni-col :span="8">
+          <button class="btn-verification"
+                  size="mini"
+                  v-show="user.getVerification=='getVerify'"
+                  @click="getAuth">获取验证码</button>
+          <button v-show="user.getVerification=='clickBanned'"
+                  size="mini"
+                  class="update-verification"
+                  style="padding:0">
+            重新获取({{user.timeSeconds}}S)
+          </button>
+        </uni-col>
+      </uni-row>
+      <view :class="error['inviteCode']?'error-inviteCode':''">
+        <input placeholder-style="color:#b1acac"
+               type="text"
+               class="input-class"
+               v-model="user.inviteCode"
+               placeholder="首次登录请输入邀请码"
+               :class="error['inviteCode']?'border-red':''" />
+      </view>
+    </view>
+    <button @click="loginCheck"
+            type="primary"
+            class="register">登录</button>
   </view>
 </template>
 
@@ -29,20 +57,16 @@ import { request } from '../../server/request.js'
 export default {
   data () {
     return {
+      error: { phone: "", verifyCode: "", inviteCode: "" },
       path: "",
       pathMin: "",
-      user: { phone: '', verifyCode: '' },
-      rules: {
-        phone: {
-          rules: [{ required: true, errorMessage: '请输入正确的用户名' }]
-        },
-        verifyCode: {
-          rules: [{ required: true, errorMessage: '请输入密码' }, {
-            minLength: 6,
-            errorMessage: '密码不得少于6位数',
-          }]
-        }
-      }
+      user: {
+        phone: "",
+        verifyCode: "",
+        inviteCode: "",
+        timeSeconds: "60",
+        getVerification: 'getVerify',
+      },
     }
   },
   created () {
@@ -53,10 +77,47 @@ export default {
     this.pathMin = option.path
   },
   methods: {
+    // 短信验证
+    getAuth () {
+      if ((/^1(3|4|5|6|7|8|9)\d{9}$/.test(this.user.phone))) {
+        this.user.getVerification = 'clickBanned'
+        let item = setInterval(() => {
+          if (this.user.timeSeconds > 1) {
+            this.user.timeSeconds--
+          } else {
+            this.user.getVerification = 'getVerify'
+            this.user.timeSeconds = 60
+            clearInterval(item)
+          }
+        }, 1000);
+        request({
+          url: '/sms/sendVerifyCode',
+          method: "post",
+          data: {
+            phone: this.user.phone
+          }
+        })
+          .then((loadresult) => {
+            if (loadresult.data.code) {
+              uni.showToast({
+                title: "验证码发送成功",
+                icon: "none"
+              })
+            }
+          })
+      } else {
+        uni.showToast({
+          title: "手机号码输入错误",
+          icon: "none"
+        })
+      }
+    },
     getStorageSyncData () {
       if (uni.getStorageSync("userData")) {
         let userList = JSON.parse(uni.getStorageSync("userData"))
-        this.user = userList
+        let { phone, verifyCode } = userList
+        this.user.phone = phone
+        this.user.verifyCode = verifyCode
       }
       if (uni.getStorageSync("pathParameter")) {
         let pathParameter = uni.getStorageSync('pathParameter')
@@ -67,49 +128,125 @@ export default {
         this.path = path.substring(0, path.length - 1)
       }
     },
+    loginCheck () {
+      let { phone, verifyCode, inviteCode } = this.user
+      if (!phone || !verifyCode) {
+        if (!phone) {
+          this.error.phone = "true"
+        }
+        if (!verifyCode) {
+          this.error.verifyCode = "true"
+        }
+      } else {
+        this.submitForm()
+      }
+    },
     submitForm () {
-      this.$refs.form.submit().then((res) => {
-        request({
-          url: '/sms/verification',
-          method: 'post',
-          data: res
-        })
-          .then((loadresult) => {
-            let { code, message, data } = loadresult.data
-            uni.showToast({ title: message, icon: 'none' })
-            if (code === 200) {
-              let user = JSON.stringify(res)
-              uni.setStorageSync("userData", user)
-              uni.setStorageSync('token', data.token)
-              setTimeout(() => {
-                if (this.pathMin) {
-                  uni.reLaunch({ url: this.pathMin })
-                  return null
-                }
-                if (this.path && this.path != "pages/login/login") {
-                  uni.reLaunch({ url: this.path })
-                } else {
-                  uni.reLaunch({ url: "/pages/index/index" })
-                }
-              }, 2000)
-            }
-          })
+      let { phone, verifyCode, inviteCode } = this.user
+      let res = { phone: phone, verifyCode: verifyCode, proxyCode: inviteCode, verifyType: '1' }
+      console.log(res)
+      request({
+        url: '/sms/verification',
+        method: 'post',
+        data: res
       })
+        .then((loadresult) => {
+          let { code, message, data } = loadresult.data
+          console.log(message)
+          if (message == "邀请码错误") {
+            this.error.inviteCode = "true"
+          }
+          uni.showToast({ title: message, icon: 'none' })
+          if (code === 200) {
+            let user = JSON.stringify(res)
+            uni.setStorageSync("userData", user)
+            uni.setStorageSync('token', data.token)
+            setTimeout(() => {
+              if (this.pathMin) {
+                uni.reLaunch({ url: this.pathMin })
+                return null
+              }
+              if (this.path && this.path != "pages/login/login") {
+                uni.reLaunch({ url: this.path })
+              } else {
+                uni.reLaunch({ url: "/pages/index/index" })
+              }
+            }, 2000)
+          }
+        })
     }
   }
 }
 </script>
 
 <style>
+page {
+  background-color: #f5f5f5;
+}
 .login-box {
-  padding: 40rpx;
+  padding: 20px;
   font-size: 28rpx;
 }
-.login-header {
-  font-weight: bold;
-  font-size: 48rpx;
-  line-height: 80rpx;
-  text-align: center;
-  margin-bottom: 20rpx;
+.login-logo {
+  width: 200rpx;
+  height: 200rpx;
+  margin: 0 auto;
+}
+.login-logo img {
+  width: 100%;
+  height: 100%;
+}
+.input-list {
+  margin: 50px 0;
+}
+.input-class {
+  margin-bottom: 30px;
+  padding-left: 10px;
+  height: 40px;
+  background-color: #fff;
+  border-radius: 5px;
+}
+.update-verification,
+.btn-verification {
+  height: 40px;
+  line-height: 40px;
+  width: 100%;
+}
+.btn-verification {
+  background-color: #409eff;
+  color: #fff;
+}
+.update-verification {
+  background-color: #bbbbbb;
+  color: #fff;
+}
+.register {
+  background-color: #409eff;
+}
+.error-phone,
+.error-verifyCode,
+.error-inviteCode {
+  position: relative;
+}
+.error-phone::after,
+.error-verifyCode::after,
+.error-inviteCode::after {
+  position: absolute;
+  top: 42px;
+  left: 2px;
+  color: red;
+}
+.error-phone::after {
+  content: "请输入用户名";
+}
+.error-verifyCode::after {
+  content: "请输入密码/验证码";
+}
+.error-inviteCode::after {
+  content: "请输入邀请码";
+}
+.login-box .border-red {
+  border: 1px solid red;
+  box-sizing: border-box;
 }
 </style>
