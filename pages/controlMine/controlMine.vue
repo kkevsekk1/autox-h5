8827aef780ed7f1e3affd7c29ee5a69cabff4921
程书伦 <chengshulun@live.com>
2021-06-22 20:20:32 +0800

    转换content 代码

--- pages/controlMine/controlMine.vue
+++ pages/controlMine/controlMine.vue
@@ -2,123 +2,117 @@
   <view class="controlMine-box">
     <view class="head">
       <uni-row class="user-head">
-        <uni-col :span="4"
-                 class="img-logo">
-          <img :src="logoImg"
-               alt="">
+        <uni-col :span="4" class="img-logo">
+          <img :src="logoImg" alt="" />
         </uni-col>
-        <uni-col :span="15"
-                 class="name-code"
-                 style="padding: 10px 0 0 15px;">
+        <uni-col :span="15" class="name-code" style="padding: 10px 0 0 15px">
           <view>
-            {{ mineData.name}}
+            {{ mineData.name }}
           </view>
-          <view>
-            您好，您的邀请码: {{ mineData.code}}
-          </view>
+          <view> 您好，您的邀请码: {{ mineData.code }} </view>
         </uni-col>
-        <uni-col :span="5"
-                 class="role"
-                 style="padding-top:10px">
-          <text style="float:right"> {{mineData.role}} </text>
+        <uni-col :span="5" class="role" style="padding-top: 10px">
+          <text style="float: right"> {{ mineData.role }} </text>
         </uni-col>
       </uni-row>
       <uni-row class="money">
-        <uni-col :span="8"
-                 class="money-balance">
-          <view> {{mineData.balance}}A币 </view>
+        <uni-col :span="8" class="money-balance">
+          <view> {{ mineData.balance }}A币 </view>
           <view>用户余额</view>
         </uni-col>
       </uni-row>
     </view>
     <view class="nav">
       <uni-row class="nav-list">
-        <uni-col :xs="8"
-                 :sm="6"
-                 :md="4"
-                 v-for="(functionData,index) in then"
-                 :key="index">
-          <functionItem :functionData="functionData"
-                        class="nav-list-number"> </functionItem>
+        <uni-col
+          :xs="8"
+          :sm="6"
+          :md="4"
+          v-for="(functionData, index) in then"
+          :key="index"
+        >
+          <functionItem :functionData="functionData" class="nav-list-number">
+          </functionItem>
         </uni-col>
       </uni-row>
     </view>
-    <button class="btn-logout"
-            type="default"
-            @click="logOff ">退出登录</button>
+    <button class="btn-logout" type="default" @click="logOff">退出登录</button>
   </view>
 </template>
 
 <script>
-import { request } from '../../server/request.js';
-import functionItem from "./functionItem"
+import { request } from '../../server/request.js'
+import functionItem from './functionItem'
 export default {
   components: {
     functionItem,
   },
-  data () {
+  data() {
     return {
       mineData: '',
-      logoImg: "../../static/portrait.png",
+      logoImg: '../../static/portrait.png',
       then: [],
     }
   },
-  created () {
+  created() {
     this.getMineData()
     this.getColumn('首页功能')
   },
   methods: {
-    getMineData () {
+    getMineData() {
       uni.showLoading({
-        title: '加载中'
-      });
+        title: '加载中',
+      })
       request({
         url: '/auth/userInfoApp',
         method: 'get',
         data: '',
-      })
-        .then(res => {
-          let { code, data } = res.data
-          uni.hideLoading()
-          if (code == 200) {
-            let { name, code, balance, role } = data
-            role = role == '7' ? '普通用户' : '代理商'
-            this.mineData = {
-              name: name,
-              code: code,
-              role: role,
-              balance: balance
-            }
+      }).then((res) => {
+        let { code, data } = res.data
+        uni.hideLoading()
+        if (code == 200) {
+          let { name, code, balance, role } = data
+          role = role == '7' ? '普通用户' : '代理商'
+          this.mineData = {
+            name: name,
+            code: code,
+            role: role,
+            balance: balance,
           }
-        })
-    },
-    logOff () {
-      uni.reLaunch({
-        url: "/pages/login/login?path=" + this.$route.path
+        }
       })
     },
-    getColumn (name) {
+    logOff() {
+      uni.reLaunch({
+        url: '/pages/login/login?path=' + this.$route.path,
+      })
+    },
+    convertContent(content) {
+      content = content.replace('{{code}}', this.mineData.code)
+      return content
+    },
+    getColumn(name) {
       request({
         url: '/program/getByName?name=' + name,
         method: 'get',
-      })
-        .then(res => {
-          let { code, data } = res.data
-          if (code == 200) {
-            data.children.forEach(element => {
-              let { type, name, content, icon1, icon2 } = element
-              this.then.push({
-                type: type,
-                imgPath: icon1 || icon2,
-                functionName: name,
-                path: content,
-              })
+      }).then((res) => {
+        let { code, data } = res.data
+        if (code == 200) {
+          data.children.forEach((element) => {
+            let { type, name, content, icon1, icon2 } = element
+            content = this.convertContent(content)
+            this.then.push({
+              type: type,
+              imgPath: icon1 || icon2,
+              functionName: name,
+              path: content,
             })
-            console.log(this.then, "then")
-          }
-        })
-    }
-  }
+          })
+          console.log(this.then, 'then')
+        }
+      })
+    },
+  },
 }
 </script>
 
