a6b7bd57c92ad0a45f734e0b7413dbcf2b99c937
toinghub <228530402@qq.com>
2021-06-23 09:58:03 +0800

    特价品个人主页

---
+++ pages/onSaleMine/functionItem.vue
@@ -0,0 +1,103 @@
+<template>
+  <view @click="pitchOn">
+    <view class="nav-type">
+      <view>
+        <img :src="functionData.imgPath"
+             alt="">
+      </view>
+      <view> {{functionData.functionName}} </view>
+    </view>
+    <uni-popup ref="popup"
+               type="center">
+      <view class="list">
+        <view>内容已复制</view>
+        <textarea v-model="functionData.path" />
+        <view @click="closePopup"> <img src="../../static/btn_skip.png"
+               alt=""> </view>
+      </view>
+    </uni-popup>
+  </view>
+</template>
+
+<script>
+import { setClipboardData } from '@/uni_modules/u-clipboard/js_sdk'
+export default {
+  props: ["functionData"],
+  methods: {
+    pitchOn () {
+      let { type, path } = this.functionData
+      if (type === "内部链接") {
+        uni.navigateTo({
+          url: path
+        })
+      }
+      if (type === "外部链接") {
+        window.location.href = path
+      }
+      if (type === '复制功能') {
+        this.openPopup()
+      }
+    },
+    openPopup () {
+      setClipboardData(this.functionData.path);
+      this.$refs.popup.open();
+    },
+    closePopup () {
+      this.$refs.popup.close()
+    },
+  }
+}
+</script>
+
+<style>
+.nav-type {
+  transform: translateY(50%);
+}
+.nav-type view:nth-child(1) {
+  display: inline-block;
+  width: 40px;
+  height: 40px;
+  max-width: 60px;
+  max-height: 60px;
+  margin-bottom: 10px;
+}
+.nav-type view:nth-child(1) img {
+  width: 100%;
+  height: 100%;
+}
+.nav .nav-type view:nth-child(2) {
+  font-size: 14px;
+  font-weight: 700;
+}
+.list {
+  width: 310px;
+  height: 265px;
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+.list view {
+  margin: 0 auto;
+}
+.list view:nth-child(1) {
+  font-size: 16px;
+  text-align: center;
+}
+.list textarea {
+  margin: 0 auto;
+  width: 250px;
+  height: 125px;
+  padding: 10px;
+  font-size: 14px;
+  background-color: #f5f5f5;
+}
+.list view:nth-child(3) {
+  width: 200px;
+}
+.list view:nth-child(3) img {
+  width: 100%;
+}
+</style>
---
+++ pages/onSaleMine/onSaleMine.vue
@@ -0,0 +1,183 @@
+<template>
+  <view class="controlMine-box">
+    <view class="head">
+      <uni-row class="user-head">
+        <uni-col :span="4"
+                 class="img-logo">
+          <img :src="logoImg"
+               alt="" />
+        </uni-col>
+        <uni-col :span="15"
+                 class="name-code"
+                 style="padding: 10px 0 0 15px">
+          <view>
+            {{ mineData.name }}
+          </view>
+        </uni-col>
+        <!-- <uni-col :span="5"
+                 class="role"
+                 style="padding-top: 10px">
+          <text style="float: right"> {{ mineData.role }} </text>
+        </uni-col> -->
+      </uni-row>
+    </view>
+    <view class="nav">
+      <uni-row class="nav-list">
+        <uni-col :xs="8"
+                 :sm="6"
+                 :md="4"
+                 v-for="(functionData, index) in then"
+                 :key="index">
+          <functionItem :functionData="functionData"
+                        class="nav-list-number">
+          </functionItem>
+        </uni-col>
+      </uni-row>
+    </view>
+    <button class="btn-logout"
+            type="default"
+            @click="logOff">退出登录</button>
+  </view>
+</template>
+
+<script>
+import { request } from '../../server/request.js'
+import functionItem from './functionItem'
+export default {
+  components: {
+    functionItem,
+  },
+  data () {
+    return {
+      mineData: '',
+      logoImg: '../../static/portrait.png',
+      then: [],
+    }
+  },
+  created () {
+    this.getMineData()
+    this.getColumn('特价品')
+  },
+  methods: {
+    getMineData () {
+      uni.showLoading({
+        title: '加载中',
+      })
+      request({
+        url: '/auth/userInfoApp',
+        method: 'get',
+        data: '',
+      }).then((res) => {
+        let { code, data } = res.data
+        uni.hideLoading()
+        if (code == 200) {
+          let { name, code, balance, role } = data
+          role = role == '7' ? '普通用户' : '代理商'
+          this.mineData = {
+            name: name,
+            code: code,
+            role: role,
+            balance: balance,
+          }
+        }
+      })
+    },
+    logOff () {
+      uni.reLaunch({
+        url: '/pages/login/login?path=' + this.$route.path,
+      })
+    },
+    convertContent (content) {
+      content = content.replace('{{code}}', this.mineData.code)
+      return content
+    },
+    getColumn (name) {
+      request({
+        url: '/program/getByName?name=' + name,
+        method: 'get',
+      }).then((res) => {
+        let { code, data } = res.data
+        if (code == 200) {
+          data.children.forEach((element) => {
+            let { type, name, content, icon1, icon2 } = element
+            content = this.convertContent(content)
+            this.then.push({
+              type: type,
+              imgPath: icon1 || icon2,
+              functionName: name,
+              path: content,
+            })
+          })
+          console.log(this.then, 'then')
+        }
+      })
+    },
+  },
+}
+</script>
+
+<style>
+page {
+  background-color: #f5f5f5;
+}
+.controlMine-box {
+  padding: 10px;
+  background: url(../../static/controlBack.png) no-repeat;
+  background-size: 100% 170px;
+}
+.user-head {
+  height: 70;
+  margin: 10px 0 15px 0;
+  font-size: 16px;
+  font-weight: 700;
+  color: #fff;
+}
+.img-logo {
+  max-width: 70px;
+}
+.img-logo img {
+  width: 100%;
+  height: 100%;
+}
+.name-code view:nth-child(2) {
+  font-size: 16px;
+  margin-top: 5px;
+}
+.money {
+  display: flex;
+  align-items: center;
+  height: 80px;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 0px 0px 8px #888888;
+}
+.money-balance {
+  text-align: center;
+}
+.money-balance view:nth-child(1) {
+  margin-bottom: 4px;
+  font-size: 16px;
+  font-weight: 700;
+}
+.money-balance view:nth-child(2) {
+  font-size: 14px;
+  color: #999;
+}
+.nav {
+  margin: 40px 0;
+}
+.nav-list-number {
+  height: 120px;
+  text-align: center;
+  background-color: #fff;
+  border: 2px solid #f5f5f5;
+}
+.btn-logout {
+  background-color: #409eff;
+  color: white;
+  margin-top: 22px;
+  font-size: 16px;
+  height: 50px;
+  line-height: 50px;
+}
+</style>
