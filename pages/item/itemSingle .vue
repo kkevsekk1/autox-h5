95d01fe0fd5f9f370a6b7dc3f4b0edbdd1672493
toinghub <228530402@qq.com>
2021-06-23 15:55:28 +0800

    库存设置

---
+++ pages/item/itemSingle .vue
@@ -0,0 +1,85 @@
+<template>
+  <view>
+    <uni-row>
+      <uni-col :span="8">
+        <img src="../../static/logo.png"
+             class="title-img"
+             alt="">
+      </uni-col>
+      <uni-col :span="16">
+        <view>
+          <text class="title">{{item.title}}</text>
+          <text style="float:right;margin-right:8px;"> {{item.status}} </text>
+        </view>
+        <uni-row style="margin-top:10px">
+          <uni-col :span="16"
+                   style="font-size: 14px;">
+            <view> {{ item.subtitle}} / {{item.unit}} </view>
+            <view>剩余：{{item.surplusStock}}{{item.unit}}</view>
+            <view>库存：{{item.totalStock}}{{item.unit}} </view>
+          </uni-col>
+          <uni-col :span="8">
+            <button size='mini'
+                    class="btn-set"
+                    style="float:right"
+                    @click="setItem(item.id)">基础</button>
+            <button size='mini'
+                    class="btn-repertory"
+                    style="float:right;margin-top:3px;"
+                    @click="popupRepertory(item)">库存</button>
+          </uni-col>
+        </uni-row>
+      </uni-col>
+    </uni-row>
+    <uni-row style="margin-top:10px;font-size:14px;">
+      <uni-col :span="8">
+        <view>普通价：{{item.sellingPrice}} </view>
+        <view>会员价：{{item.vipPrice}}</view>
+      </uni-col>
+      <uni-col :span="8">
+        <view>代理价：{{item.proxyPrice}}</view>
+        <view>进货价：{{item.jinPrice}} </view>
+      </uni-col>
+      <uni-col :span="8">
+        <view>正价：{{item.originalPrice}}</view>
+        <view>备用价：{{item.remarkPrice}}</view>
+      </uni-col>
+    </uni-row>
+  </view>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  methods: {
+    setItem (id) {
+      this.$emit("setItem", id)
+    },
+    popupRepertory (item) {
+      let data = {
+        id: item.id,
+        totalStock: item.totalStock,
+        unit: item.unit,
+        status: item.status
+      }
+      this.$emit("popupRepertory", data)
+    }
+  }
+}
+</script>
+
+<style>
+.title-img {
+  width: 90px;
+  height: 90px;
+}
+.title {
+  font-size: 16px;
+  font-weight: 700;
+}
+.btn-repertory,
+.btn-set {
+  background: #409eff;
+  color: #fff;
+}
+</style>
--- pages/item/items.vue
+++ pages/item/items.vue
@@ -1,89 +1,298 @@
 <template>
-  <view style="padding:8px" >
-<uni-row class="demo-uni-row" :gutter=20 >
-    <uni-col :span="18">
-    <view >
-			<uni-easyinput  v-model="search" placeholder="请输入内容"></uni-easyinput>
-		</view>
-    </uni-col>
-    <uni-col :span="6" style="text-align:left;">
-      <button class="mini-btn" type="primary" size="mini" @click="scanBarcode" >扫码</button>
-    </uni-col>
-</uni-row>
-<uni-row class="demo-uni-row">
-    <uni-col :span="24" v-if="datas.length>0" >
-    <view v-for="item,index in datas" :key="index">
-        <view>{{item.title}}</view>
+  <view>
+    <view class="head">
+      <uni-row class="demo-uni-row "
+               :gutter=20>
+        <uni-col :span="18">
+          <view>
+            <uni-easyinput v-model="search"
+                           style="background-color:#fff"
+                           placeholder="请输入内容"></uni-easyinput>
+          </view>
+        </uni-col>
+        <uni-col :span="6"
+                 style="text-align:left;">
+          <button class="mini-btn"
+                  style="background-color:#409eff;"
+                  type="primary"
+                  size="mini"
+                  @click="scanBarcode">扫码</button>
+        </uni-col>
+      </uni-row>
     </view>
-  </uni-col>
-    <uni-col :span="24" v-if="datas.length==0" >
+    <uni-row class="demo-uni-row content">
+      <uni-col :span="24"
+               v-if="datas.length>0">
+        <view v-for="item,index in datas"
+              :key="index">
+          <item-single :item="item"
+                       class="item-single"
+                       @popupRepertory="popupRepertory"></item-single>
+          <!-- @setItem="setItem" -->
+
+        </view>
+      </uni-col>
+      <uni-col :span="24"
+               v-if="datas.length==0">
         <view style="text-align:center;padding-top:20px;">暂无内容</view>
-  </uni-col>
-</uni-row>
+      </uni-col>
+    </uni-row>
+    <!-- 库存弹窗 -->
+    <uni-popup ref="popup"
+               type="center">
+      <view class="popupRepertory">
+        <view class="popup-title">库存设置</view>
+        <view class="popup-content">
+          <view class="popup-nav">
+            <text>库存：</text>
+            <input type='number'
+                   v-model="itemSingle.totalStock"
+                   placeholder-style="font-size:14px"
+                   placeholder="请输入数量" />
+            <text style="margin-left:5px"> {{itemSingle.unit}} </text>
+          </view>
+          <view class="popup-nav">
+            <text>状态：</text>
+            <picker @change="bindPickerChange"
+                    :value="index"
+                    :range="array"
+                    class="picker">
+              <view style="font-size:14px">{{array[index]}}</view>
+              <text class="iconfont popup-icon">&#xe603;</text>
+            </picker>
+          </view>
+        </view>
+        <uni-row>
+          <uni-col :span="12">
+            <button size="mini"
+                    style="float:right;margin-right:10px"
+                    @click="$refs.popup.close()">取消</button>
+          </uni-col>
+          <uni-col :span="12">
+            <button size="mini"
+                    class="popup-save"
+                    @click="saveRepertory">保存</button>
+          </uni-col>
+        </uni-row>
+      </view>
+    </uni-popup>
   </view>
 </template>
 <script>
 import { request } from '../../server/request.js'
+import itemSingle from './itemSingle '
 import { formatTime } from '../../utils/format.js'
 export default {
+  components: { itemSingle },
   data () {
     return {
-      search:'',
-       datas:[],
-       timer:'',
+      search: '',
+      datas: [],
+      timer: '',
+      fid: '51',
+      index: '',
+      array: ['上架', '下架'],
+      arrays: {
+        '上架': 1,
+        '下架': 0
+      },
+      itemSingle: "",
+      page: { index: 1, size: 20, pages: 2 },
+      orderby: '',
+      searchText: '',
+      type: "",
+      statuss: {
+        0: '已上架',
+        1: '已下架',
+      },
+
     }
   },
   watch: {
     search () {
-      this.debounce(300,this.loadData)
+      this.debounce(300, this.loadData)
     }
   },
   created () {
-   
+    this.fetchData();
   },
   onReachBottom () {
-    // 当前页大于等于总页数
-
+    if (this.page.index >= this.page.pages) {
+      uni.showToast({ title: '到底了', iccon: 'none' })
+    } else {
+      this.page.index++
+      this.loadFeatures();
+    }
+  },
+  onPullDownRefresh () {
+    this.page.index = 1;
+    this.features = [];
+    this.loadFeatures();
+    setTimeout(() => {
+      uni.stopPullDownRefresh()
+    }, 1000)
   },
   methods: {
-    scanBarcode(){
-        console.log(jssdk)
-        this.search="123"
+    fetchData () {
+      this.datas = []
+      const data = {
+        index: this.page.index,
+        size: this.page.size,
+        orderBy: this.orderby || "id desc",
+        search: this.searchText,
+        fid: this.fid,
+        type: this.type || '-1'
+      };
+      request({
+        url: "/item/items",
+        method: "post",
+        data,
+      })
+        .then(res => {
+          let { code, data: { index, size, pages, list } } = res.data
+          if (code === 200) {
+            this.page = { index: Number(index), size: Number(size), pages: Number(pages), };
+            list.forEach(element => {
+              element.status = this.statuss[element.status]
+            })
+            this.datas = list
+          }
+        })
     },
-    debounce (wait,fun) {
+    scanBarcode () {
+      console.log(jssdk)
+      this.search = "123"
+    },
+    debounce (wait, fun) {
       if (this.timer) {
         clearInterval(this.timer)
       }
       this.timer = setTimeout(() => {
-       fun();
+        fun();
       }, wait)
     },
-   loadData () {
-     if(!this.search){
-       console.log('不允许搜索词为空',this.search,'333')
-       return ;
-     }
+    loadData () {
+      if (!this.search) {
+        console.log('不允许搜索词为空', this.search, '333')
+        return;
+      }
       uni.showLoading({
         title: '加载中',
       })
       request({
-        url: '/item/findItems?search='+this.search,
+        url: '/item/findItems?search=' + this.search,
         method: 'get',
       }).then((res) => {
-        this.datas=[];
+        this.datas = [];
         uni.hideLoading()
         const { message, code, data } = res.data
         if (code === 200) {
           data.forEach((item) => {
+            item.status = this.statuss[item.status]
             this.datas.push(item)
           })
         }
       })
     },
-  }     
+    bindPickerChange: function (e) {
+      this.index = e.target.value
+    },
+    popupRepertory (data) {
+      this.itemSingle = data
+      this.index = data.status === '已上架' ? 0 : 1;
+      this.$refs.popup.open()
+    },
+    saveRepertory () {
+      let data = {
+        id: this.itemSingle.id,
+        status: this.arrays[this.array[this.index]],
+        totalStock: this.itemSingle.totalStock
+      }
+      request({
+        url: "/item/setStatus",
+        method: "post",
+        data,
+      })
+        .then(res => {
+          let { code, message } = res.data
+          if (code == 200) {
+            uni.showToast({
+              title: message
+            })
+            this.$refs.popup.close()
+          } else {
+            uni.showToast({
+              titel: message,
+              icon: "none"
+            })
+          }
+        })
+    }
+  }
 }
 </script>
 
 <style>
-
+page {
+  background-color: #f5f5f5;
+}
+.head {
+  width: 100%;
+  padding: 8px;
+  background: #f5f5f5;
+  box-sizing: border-box;
+  position: fixed;
+  z-index: 9;
+}
+.content {
+  padding-top: 55px;
+}
+.item-single {
+  background-color: #fff;
+  padding: 10px;
+}
+.popupRepertory {
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 5px;
+}
+.popup-title {
+  text-align: center;
+  font-size: 16px;
+  font-weight: 700;
+}
+.popup-content {
+  margin: 15px 0 30px 0;
+  font-size: 14px;
+}
+.popup-save {
+  margin-left: 10px;
+  background: #409eff;
+  color: #fff;
+}
+.popup-nav {
+  display: flex;
+  height: 30px;
+  line-height: 30px;
+  margin-bottom: 10px;
+}
+.popup-nav input,
+.popup-nav .picker {
+  width: 175px;
+  height: 30px;
+  padding-left: 5px;
+  border-radius: 3px;
+  border: 1px solid #dddddd;
+}
+.popup-nav .picker {
+  position: relative;
+}
+.popup-icon {
+  position: absolute;
+  right: 10px;
+  top: 4px;
+  transform: rotate(90deg);
+  font-size: 12px;
+  color: #848484;
+}
 </style>
