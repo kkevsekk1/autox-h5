a2cd7809378dc59dcd4c701bd9c97a2f98e27d6a
程书伦 <chengshulun@live.com>
2021-06-24 16:08:30 +0800

    修改布局

--- pages/order/preOrderItem.vue
+++ pages/order/preOrderItem.vue
@@ -1,45 +1,43 @@
 <template>
   <view class="preOrderItem">
-    <view class="title">
-      <text>{{item.title}}</text>
-      <view class="iconfont icon-delete"
-            @click="deleteItem">&#xe62f;</view>
-    </view>
-    <uni-row :gutter=20
-             class="content">
-      <uni-col :span="8"
-               class="col-class">
-        <text>{{item.subTitle}}</text>
+    <uni-row :gutter="20" class="content">
+      <uni-col :span="24" class="col-class">
+        <view class="title">
+          <text>{{ item.title }}</text>
+          <view class="iconfont icon-delete" @click="deleteItem">&#xe62f;</view>
+        </view>
       </uni-col>
-      <uni-col :span="8"
-               class="col-class">
-        <text> 库存： {{item.surplusStock}}{{item.unit}}</text>
+    </uni-row>
+    <uni-row :gutter="20" class="content">
+      <uni-col :span="8" class="col-class">
+        <text>{{ item.subTitle }}</text>
       </uni-col>
-      <uni-col :span="8"
-               class="col-class">
-        <text style="color:red">单价：{{univalence}}</text>
+      <uni-col :span="8" class="col-class">
+        <text> 库存： {{ item.surplusStock }}{{ item.unit }}</text>
       </uni-col>
-      <uni-col :span="8"
-               class="col-class">
-        <text>剩余：{{surplusTime}} 天</text>
+      <uni-col :span="8" class="col-class">
+        <text style="color: red">单价：{{ univalence }}</text>
       </uni-col>
-      <uni-col :span="8"
-               class="col-class">
+    </uni-row>
+    <uni-row>
+      <uni-col :span="8" class="col-class">
+        <text>剩余：{{ surplusTime }} 天</text>
+      </uni-col>
+      <uni-col :span="8" class="col-class">
         <uni-row>
-          <uni-col :span="10">
-            数量：
-          </uni-col>
+          <uni-col :span="10"> 数量： </uni-col>
           <uni-col :span="14">
-            <input v-model="item.purchaseNumber"
-                   style="font-size:14px;color:red;"
-                   placeholder-style="font-size:14px;"
-                   placeholder="数量">
+            <input
+              v-model="item.purchaseNumber"
+              style="font-size: 14px; color: red"
+              placeholder-style="font-size:14px;"
+              placeholder="数量"
+            />
           </uni-col>
         </uni-row>
       </uni-col>
-      <uni-col :span="8"
-               class="col-class">
-        <text>小计：{{sum}} 元</text>
+      <uni-col :span="8" class="col-class">
+        <text>小计：{{ sum }} 元</text>
       </uni-col>
     </uni-row>
   </view>
@@ -49,55 +47,54 @@
 export default {
   props: ['item', 'userType'],
   computed: {
-    univalence () {
+    univalence() {
       let univalences = {
-        '普通': 'sellingPrice',
-        '会员': 'vipPrice',
-        '代理': 'proxyPrice',
+        普通: 'sellingPrice',
+        会员: 'vipPrice',
+        代理: 'proxyPrice',
       }
       return this.item[univalences[this.userType]] || ''
     },
-    surplusTime () {
+    surplusTime() {
       if (!this.item.endTime) {
         return
       }
       let now = new Date()
       let date = this.item.endTime.slice(0, 10)
       let until = new Date(date)
-      let days = ((until - now) / 1000 / 3600 / 24) + 1;
-      return Math.floor(days);
+      let days = (until - now) / 1000 / 3600 / 24 + 1
+      return Math.floor(days)
     },
-    sum () {
+    sum() {
       let sum = this.item.purchaseNumber * this.univalence
       this.subtotal(sum)
       return sum
-    }
+    },
   },
-  data () {
-    return {
-    }
+  data() {
+    return {}
   },
   watch: {
-    purchaseNumber () {
+    purchaseNumber() {
       if (Number(this.item.purchaseNumber) > Number(this.item.surplusStock)) {
         this.item.purchaseNumber = ''
       }
-    }
+    },
   },
   methods: {
-    deleteItem () {
+    deleteItem() {
       this.$emit('deleteItem', this.item.id)
     },
-    subtotal (sum) {
+    subtotal(sum) {
       let data = {
         id: this.item.id,
-        sum: sum
+        sum: sum,
       }
       if (sum) {
-        this.$emit("subtotal", data)
+        this.$emit('subtotal', data)
       }
-    }
-  }
+    },
+  },
 }
 </script>
 
