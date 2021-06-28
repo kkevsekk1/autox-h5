241803331321b1e2de6d3cbd861bf02706fc1e00
toinghub <228530402@qq.com>
2021-06-28 15:55:19 +0800

    修改密码 限制最少6为位

--- pages/modifyPassword/modifyPassword.vue
+++ pages/modifyPassword/modifyPassword.vue
@@ -1,18 +1,17 @@
 <template>
   <view class="modifypassword_box">
-    <uni-forms :value="Password"
+    <uni-forms :modelValue="Password"
                :rules="rules"
                ref="form"
                calss="password-forms">
-      <uni-forms-item name="firstPassword"
-                      required>
+      <uni-forms-item name="firstPassword">
         <uni-easyinput type="text"
                        v-model="Password.firstPassword"
                        class="easyinput"
                        placeholder="请输入新密码" />
       </uni-forms-item>
       <button class="button-modify "
-              @click="getPassword"
+              @click="submit"
               type="primary">确认修改</button>
     </uni-forms>
   </view>
@@ -28,27 +27,38 @@ export default {
       },
       rules: {
         firstPassword: {
-          rules: [{
-            minLength: 6,
-            errorMessage: '密码不得少于6位数',
-          }]
+          rules: [
+            {
+              minLength: 6,
+              errorMessage: '密码长度不得少于 {minLength}位',
+            }
+          ]
         },
       }
     }
   },
   methods: {
-    getPassword (form) {
-      request({
-        url: '/auth/password',
-        method: 'post',
-        data: { password: this.Password.firstPassword },
-      })
-        .then((loadresult) => {
+    submit () {
+      this.$refs.form.submit().then(res => {
+        if (!res.firstPassword) {
           uni.showToast({
-            title: "密码修改成功",
+            title: "请输入新密码",
             icon: "none"
           })
+          return
+        }
+        request({
+          url: '/auth/password',
+          method: 'post',
+          data: { password: res.firstPassword },
         })
+          .then((loadresult) => {
+            uni.showToast({
+              title: "密码修改成功",
+              icon: "none"
+            })
+          })
+      })
     },
   }
 }
