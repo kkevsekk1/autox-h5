28d6ddcecc2e76153f2063f0c59627df887d4b97
程书伦 <chengshulun@live.com>
2021-07-02 11:05:36 +0800

    优化图片效果

--- pages/category/index.vue
+++ pages/category/index.vue
@@ -285,7 +285,7 @@ export default {
       } catch (error) { }
       if (item.pictures && item.pictures.length !== 0) {
         item.pictures.forEach((res) => {
-          this.popupPicture.pictures.push(res)
+          this.popupPicture.pictures.push(res+"_x.jpg")
         })
         this.$refs.popupPicture.open()
       }
