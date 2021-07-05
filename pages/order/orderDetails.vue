9f6c54b766ead16c512cf4ca51ad99396b8b7432
程书伦 <chengshulun@live.com>
2021-07-05 16:35:03 +0800

    支付参数修改

--- pages/order/orderDetails.vue
+++ pages/order/orderDetails.vue
@@ -179,9 +179,10 @@ export default {
     },
     toPay() {
       this.loadPayInfo().then((res) => {
-        console.log(res)
-        WeixinJSBridge.invoke('getBrandWCPayRequest', res.data, function (res) {
-          alert(res.data)
+        let {data,code,message}=res.data;
+        console.log(data)
+        WeixinJSBridge.invoke('getBrandWCPayRequest', data, function (res) {
+      
           if (res.err_msg == 'get_brand_wcpay_request:ok') {
             // 使用以上方式判断前端返回,微信团队郑重提示：
             //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
